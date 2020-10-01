ed822d6ba211de3efa30ec9b44192c52725351de
se2062020 <ycha909@aucklanduni.ac.nz>
2020-10-02 00:17:51 +1300

    Skeleton Code

--- Assignment 3/src/quinzical/Main.java
+++ Assignment 3/src/quinzical/Main.java
@@ -1,7 +1,63 @@
 package quinzical;
+	
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-public class Main {
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+
+public class Main extends Application implements EventHandler<ActionEvent>{
+
+
+	private Button btnPractice;
+	private Button btnGame;
+	
+	@Override
+	public void start(Stage primaryStage) {	
+		primaryStage.setTitle("Quinzical");
+		        
+        btnPractice = new Button();
+        btnPractice.setText("               Practice");
+        
+        btnGame = new Button();
+        btnGame.setText("Game");
+        
+        btnGame.setOnAction(this);
+        btnPractice.setOnAction(this);
+        
+        StackPane layout = new StackPane();
+        layout.getChildren().add(btnPractice);
+        layout.getChildren().add(btnGame);
+        
+		Scene scene = new Scene(layout,500,400);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	@Override
+	public void handle(ActionEvent Event) {
+		if (Event.getSource() == btnGame) {
+			System.out.println("game started.");
+		}
+		else if (Event.getSource() == btnPractice) {
+			System.out.println("practice started.");
+		}
+	}
+
+
+	
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		launch(args);
 	}
 }
