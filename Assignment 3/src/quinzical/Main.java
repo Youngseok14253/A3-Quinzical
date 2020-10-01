4cf6b26b0c16478268fd7292c75ec2ecae9ff242
se2062020 <dlee432@aucklanduni.ac.nz>
2020-10-02 12:12:39 +1300

    making practice, game and exit buttons on main menu and basicscene setup

--- Assignment 3/src/quinzical/Main.java
+++ Assignment 3/src/quinzical/Main.java
@@ -1,6 +1,7 @@
 package quinzical;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -11,6 +12,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -19,39 +21,67 @@ import javafx.event.ActionEvent;
 
 public class Main extends Application{
 
-
-	private Button btnPractice;
-	private Button btnGame;
+	Scene mainScene, gameScene, practiceScene;
 
 	@Override
 	public void start(Stage primaryStage) {	
-		primaryStage.setTitle("Quinzical");
+		
+		Label label1= new Label("Welcome to Quinzical! Please select one of the three options to continue.");
 
-		btnPractice = new Button();
-		btnPractice.setText("               Practice");
-
-		btnGame = new Button();
-		btnGame.setText("Game");
-
+		Button btnMainToGame= new Button("main menu button to game scene");
+		btnMainToGame.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				primaryStage.setScene(gameScene);
+			}
+		});
+		Button btnMainToPractice= new Button("main menu button to practice scene");
+		btnMainToPractice.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				primaryStage.setScene(practiceScene);
+			}
+		});
+		Button btnMainToExit= new Button("main menu button to exit scene");
+		btnMainToExit.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				Platform.exit();
+			}
+		});
+		//mainScene layout with VBox
+		VBox mainLayout= new VBox(20);
+		mainLayout.getChildren().addAll(label1, btnMainToPractice, btnMainToGame, btnMainToExit);
+		mainScene= new Scene(mainLayout, 500,400);
+		
+		//Making btnGame
+		Button btnGame= new Button("game button to main scene");
 		btnGame.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("testing for eventhandler");
+				primaryStage.setScene(mainScene);
 			}
 		});
+		//gameScene layout with StackPane
+		StackPane layout2= new StackPane();
+		layout2.getChildren().add(btnGame);
+		gameScene= new Scene(layout2, 600, 300);
+		
+		//Making btnPractice
+		Button btnPractice= new Button("practice button to main scene");
 		btnPractice.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("testing for eventhandler p[ractice");
+				primaryStage.setScene(mainScene);
 			}
 		});
+		//practiceScene layout with StackPane
+		StackPane layout3= new StackPane();
+		layout3.getChildren().add(btnPractice);
+		practiceScene= new Scene(layout3, 600,300);
 
-		StackPane layout = new StackPane();
-		layout.getChildren().add(btnPractice);
-		layout.getChildren().add(btnGame);
-
-		Scene scene = new Scene(layout,500,400);
-		primaryStage.setScene(scene);
+		primaryStage.setScene(mainScene);
+		primaryStage.setTitle("Quinzical");
 		primaryStage.show();
 	}
 
