e4ef8883b71c4059e48f6b50df8cd535c6e947ee
se2062020 <dlee432@aucklanduni.ac.nz>
2020-10-02 15:14:12 +1300

    testing grid pane for game mode

--- Assignment 3/src/quinzical/Main.java
+++ Assignment 3/src/quinzical/Main.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -29,6 +30,7 @@ public class Main extends Application{
 		program= primaryStage;
 		Label label1= new Label("Welcome to Quinzical! Please select one of the three options to continue.");
 
+		//Play button on main menu
 		Button btnMainToGame= new Button("Play");
 		btnMainToGame.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -36,6 +38,8 @@ public class Main extends Application{
 				program.setScene(gameScene);
 			}
 		});
+		
+		//Practice button on main menu
 		Button btnMainToPractice= new Button("Practice Mode");
 		btnMainToPractice.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -43,6 +47,8 @@ public class Main extends Application{
 				program.setScene(practiceScene);
 			}
 		});
+		
+		//Exit button on main menu
 		Button btnMainToExit= new Button("Quit");
 		btnMainToExit.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -50,6 +56,9 @@ public class Main extends Application{
 				closeProgram();
 			}
 		});
+		
+		
+		//Closing when pressing the red X
 		program.setOnCloseRequest(new EventHandler<WindowEvent>() {
 			
 			@Override
@@ -59,10 +68,13 @@ public class Main extends Application{
 				
 			}
 		});
+		
+		
 		//mainScene layout with VBox
 		VBox mainLayout= new VBox(20);
 		mainLayout.getChildren().addAll(label1, btnMainToGame, btnMainToPractice, btnMainToExit);
 		mainScene= new Scene(mainLayout, 500,400);
+		
 		
 		//Making btnGame
 		Button btnGame= new Button("game button to main scene");
@@ -72,10 +84,33 @@ public class Main extends Application{
 				program.setScene(mainScene);
 			}
 		});
-		//gameScene layout with StackPane
-		StackPane layout2= new StackPane();
-		layout2.getChildren().add(btnGame);
-		gameScene= new Scene(layout2, 600, 300);
+		
+		
+		//gameScene layout with GridPane
+		GridPane grid= new GridPane();
+		grid.setPadding(new Insets(10,10,10,10));
+		grid.setVgap(8);
+		grid.setHgap(10);
+		Label nameLabel= new Label("Username:");
+		GridPane.setConstraints(nameLabel,0,0);
+		
+		Label passLabel= new Label("Password: ");
+		GridPane.setConstraints(passLabel,0,1);
+		
+		TextField nameInput= new TextField("dlee432");
+		GridPane.setConstraints(nameInput, 1, 0);
+		
+		TextField passInput = new TextField();
+		passInput.setPromptText("password");
+		GridPane.setConstraints(passInput, 1,1);
+		
+		Button loginButton= new Button("Log In");
+		GridPane.setConstraints(loginButton, 1, 2);
+		
+		grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton);
+		gameScene= new Scene(grid, 300,200);
+		program.setScene(gameScene);
+		
 		
 		//Making btnPractice
 		Button btnPractice= new Button("practice button to main scene");
@@ -85,15 +120,19 @@ public class Main extends Application{
 				program.setScene(mainScene);
 			}
 		});
+		
+		
 		//practiceScene layout with StackPane
 		StackPane layout3= new StackPane();
 		layout3.getChildren().add(btnPractice);
 		practiceScene= new Scene(layout3, 600,300);
-
 		program.setScene(mainScene);
+		
+		
 		program.setTitle("Quinzical");
 		program.show();
 	}
+	
 	private void closeProgram() {
 		Boolean answer= ConfirmBox.display("Quit", "Are you sure you want to exit?");
 		if(answer) {
