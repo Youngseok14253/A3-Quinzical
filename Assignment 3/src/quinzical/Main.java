df5992790e713c3e72b8a8cbcea7d4bf8f4fcc63
se2062020 <ycha909@aucklanduni.ac.nz>
2020-10-02 23:09:51 +1300

    Created SelectCategory class, implemented TTS RadioMenu

--- Assignment 3/src/quinzical/Main.java
+++ Assignment 3/src/quinzical/Main.java
@@ -108,29 +108,55 @@ public class Main extends Application{
 		//Button loginButton= new Button("Log In");
 		//GridPane.setConstraints(loginButton, 1, 2);
 		
-		Button Category1 = new Button("Category 1");
+		Button Category1 = new Button("Category- 1");
 		GridPane.setConstraints(Category1, 1, 1);
 		Category1.setPrefSize(200, 40);
+		String catName1 = Category1.getText();
+		System.out.println(catName1);
 		
-		Button Category2 = new Button("Category 2");
+		Category1.setOnAction(e -> System.out.println("selected category 1."));
+		
+		Button Category2 = new Button("Category- 2");
 		GridPane.setConstraints(Category2, 1, 2);
 		Category2.setPrefSize(200, 40);
+		String catName2 = Category2.getText();
+		System.out.println(catName2);
 		
-		Button Category3 = new Button("Category 3");
+		Category2.setOnAction(e -> {
+			System.out.println("selected category 2.");
+			SelectCategory.displayQuestion(catName2);
+		}
+			);
+		
+		Button Category3 = new Button("Category- 3");
 		GridPane.setConstraints(Category3, 1, 3);
 		Category3.setPrefSize(200, 40);
+		String catName3 = Category3.getText();
+		System.out.println(catName3);
 		
-		Button Category4 = new Button("Category 4");
+		Category3.setOnAction(e -> System.out.println("selected category 3."));
+		
+		Button Category4 = new Button("Category- 4");
 		GridPane.setConstraints(Category4, 1, 4);
 		Category4.setPrefSize(200, 40);
+		String catName4 = Category4.getText();
+		System.out.println(catName4);
 		
-		Button Category5 = new Button("Category 5");
+		Category4.setOnAction(e -> System.out.println("selected category 4."));
+		
+		Button Category5 = new Button("Category- 5");
 		GridPane.setConstraints(Category5, 1, 5);
 		Category5.setPrefSize(200, 40);
+		String catName5 = Category5.getText();
+		System.out.println(catName5);
+		
+		Category5.setOnAction(e -> System.out.println("selected category 5."));
 
 		gameGrid.getChildren().addAll(nameLabel, nameInput, Category1, Category2, Category3, Category4, Category5);
 		gameScene= new Scene(gameGrid, 400,300);
 		program.setScene(gameScene);
+		
+		
 		
 		//Adjusting speed of Text-to-Speech
 		Menu playbackMenu = new Menu("Playback Speed");
