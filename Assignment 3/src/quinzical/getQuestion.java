36e1374d1a2680191875eedd3e08b4c1fde953fd
se2062020 <ycha909@aucklanduni.ac.nz>
2020-10-05 03:19:32 +1300

    Retrieve Question from Category-* files

--- Assignment 3/src/quinzical/getQuestion.java
+++ Assignment 3/src/quinzical/getQuestion.java
@@ -5,38 +5,90 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class getQuestion {
 	public static String displayQuestion(String category) {
+		
+		int categoryNum = 0;
+		int lineNum = 0;
+		
 		if (category== "Places") {
-			
-			return "testing palces";
+			categoryNum = 1;
+			lineNum = 13;
 		}
 		else if (category== "Symbols") {
-			return "testing symbols";
+			categoryNum = 2;
+			lineNum = 8;
 		}
 		else if (category== "Geography") {
-			return "testing geograhy";
+			categoryNum = 3;
+			lineNum = 12;
 		}
 		else if (category== "History") {
-			return "testing history";
+			categoryNum = 4;
+			lineNum = 9;
 		}
 		else if (category== "Famous People") {
-			return "testing famous peopel";
+			categoryNum = 5;
+			lineNum = 9;
 		}
 		else if (category== "NZ Life") {
-			return "testing nzlife";
+			categoryNum = 6;
+			lineNum = 7;
 		}
 		else if (category== "Flora") {
-			return "testing flroa";
+			categoryNum = 7;
+			lineNum = 10;
 		}
 		else if (category== "Fauna") {
-			return "testing Fauna";
+			categoryNum = 8;
+			lineNum = 18;
 		}
 		else if (category== "Oddities") {
-			return "testing idditoes";
+			categoryNum = 9;
+			lineNum = 8;
 		}
-		return "testing end return";
+		
+		String global = "";
+		
+		int randNum = (int) (2 + (Math.random() * (lineNum - 2)));
+		
+		try {
+			String command = ("bash readQuestion Category-" + categoryNum + " " + randNum);
+			ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
+
+			Process process = pb.start();
+
+			BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			BufferedReader stderr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			
+			int exitStatus = process.waitFor();
+			
+			if (exitStatus == 0) {
+				String line;
+				while ((line = stdout.readLine()) != null) {
+					global = line;
+				}
+			} else {
+				String line;
+				while ((line = stderr.readLine()) != null) {
+					System.err.println(line);
+				}
+			}
+			System.out.println("should be called");
+			System.out.println(global);
+			System.out.println("called");
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return global;
+		
+		
 	}
 }
