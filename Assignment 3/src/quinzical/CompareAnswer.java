88b902212e72fa11a82d87ef5a135b31a1819f71
se2062020 <dlee432@aucklanduni.ac.nz>
2020-10-05 14:24:37 +1300

    compareAnswer now creates a new window saying correct or incorrect

--- Assignment 3/src/quinzical/CompareAnswer.java
+++ Assignment 3/src/quinzical/CompareAnswer.java
@@ -1,6 +1,5 @@
 package quinzical;
 
-import java.awt.Label;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -9,6 +8,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -19,42 +19,32 @@ import javafx.application.Application;
 public class CompareAnswer{
 	
 	public static void isAnswerPractice(TextField input, String answer, int tries) {
-		
-		//if (tries == 1) {
-			//show hint
-			//System.out.println("showing hint");
-		//}
+		String title= "Confirmation";
 		String text= input.getText();
 		text.toLowerCase();
+		System.out.println(text);
 		answer.toLowerCase();
-		if (text.contains(answer)) {
-			display("Correct!");
-			System.out.println("correct");
+		System.out.println(answer);
+		if (text.equals(answer)) {
+			display(title, "Correct!");
 		}
 		else {
-			display("Incorrect!");
-			System.out.println("incorrect");
+			display(title, "Incorrect!");
 		}
 
 	}
 	
-	public static void display(String title) {
+	public static void display(String title,String message) {
 		Stage window= new Stage();
 		window.initModality(Modality.APPLICATION_MODAL);
 		window.setTitle(title);
-		window.setMaxHeight(0);
-		window.setMaxWidth(50);
+		window.setMinWidth(200);
 		
 		Label label= new Label();
-		label.setText(title);
+		label.setText(message);
 		
 		Button btnCont= new Button("Continue");
-		VBox layout= new VBox(10);
-		//layout.getChildren().addAll(label, btnCont);
-		layout.setAlignment(Pos.CENTER);
-		Scene scene= new Scene(layout);
-		window.setScene(scene);
-		window.showAndWait();
+		
 
 		btnCont.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -62,6 +52,12 @@ public class CompareAnswer{
 				window.close();
 			}
 		});
+		VBox layout= new VBox(10);
+		layout.getChildren().addAll(label, btnCont);
+		layout.setAlignment(Pos.CENTER);
+		Scene scene= new Scene(layout);
+		window.setScene(scene);
+		window.showAndWait();
 		
 		
 	}
